d68f5702792686e5bc27bd2e73d8dbbbfbea0821
alphajack7 <atskol7@gmail.com>
2023-09-12 16:51:15 +0530

    minor change

--- app.py
+++ app.py
@@ -12,26 +12,37 @@ def index():
 
 @app.route('/upload', methods=['POST','PUT','DELETE'])
 def upload():
+
     image_file = request.files['image']
+
     quality_value=int(request.form['quality'])
+
     image_path = 'uploads/' + image_file.filename
+
     image_file.save(image_path)
     
     
 
     # Calling Python function here, passing the image path and quality argument
     output_path=compress_image(image_path,image_file.filename,quality_value)
+
     print("Your file is compressed and is downloading...")
     
     send_file(output_path, as_attachment=True)
+
     return send_file(output_path, as_attachment=True),os.remove(output_path),os.remove(image_path)
     #return 'Image uploaded and processed successfully!'
     
+    
 
 def compress_image(image_path,filename,value):
+
     image = Image.open(image_path)
+    
     output_path="uploads/compressed"+filename
+    
     image.save(output_path, optimize=True, quality=value)
+    
     return output_path
 
 # Example usage
